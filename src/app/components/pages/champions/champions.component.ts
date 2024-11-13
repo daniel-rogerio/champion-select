f0b30a6d520caaa46618847e84dac291e525294f
daniel-rogerio <danielr.xp@gmail.com>
2024-11-13 17:11:04 -0300

    Create and add the champions page component.

---
+++ src/app/components/pages/champions/champions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-champions',
+  standalone: true,
+  imports: [],
+  templateUrl: './champions.component.html',
+  styleUrl: './champions.component.css'
+})
+export class ChampionsComponent {
+
+}
