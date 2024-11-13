157a67207419fe9ed10ee64af8ec23711a8d62f9
daniel-rogerio <danielr.xp@gmail.com>
2024-11-13 17:07:24 -0300

    Remove useless import.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar'
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule, RouterLink, RouterLinkActive],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
