a182a35d9ad240deb6fd423fdf99c60a013d1ae4
daniel-rogerio <danielr.xp@gmail.com>
2024-11-19 16:23:26 -0300

    Add champions route.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ChampionComponent } from './modules/components/champion/champion.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'champion', component: ChampionComponent}
+];
