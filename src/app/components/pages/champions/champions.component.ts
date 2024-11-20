d93694c57d5a4ad6dbaeac4543f6d7c2deb32344
daniel-rogerio <danielr.xp@gmail.com>
2024-11-19 21:52:12 -0300

    Implement method to get API data.

--- src/app/components/pages/champions/champions.component.ts
+++ src/app/components/pages/champions/champions.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ChampionListService } from '../../../services/champion-list.service';
+import { ChampionData } from '../../../models/interfaces/ChampionData';
+import { Subject, takeUntil } from 'rxjs';
+import { CardChampionComponent } from '../../card-champion/card-champion.component';
+
 
 @Component({
-  selector: 'app-champions',
+  selector: 'app-champion',
   standalone: true,
-  imports: [],
+  imports: [CardChampionComponent],
   templateUrl: './champions.component.html',
   styleUrl: './champions.component.css'
 })
-export class ChampionsComponent {
+export class ChampionComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
+  champions!: ChampionData;
+  championsList!: any;
+  splashArt!: any;
 
+  constructor(private championListService: ChampionListService) { }
+
+  ngOnInit(): void {
+    this.getChampions();
+  }
+
+  getChampions(): void {
+    this.championListService.getAll()
+    .pipe(takeUntil(this.destroy$))
+    .subscribe({
+      next: (response) => {
+        response && (this.champions = response);
+        this.championsList = Object.values(this.champions.data)
+      },
+      error: (error) => console.log(error),
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
