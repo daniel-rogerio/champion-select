128522506be2642b99d519a5a6d203340360d8ca
daniel-rogerio <danielr.xp@gmail.com>
2024-11-19 21:53:26 -0300

    Add the new component to champions route.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { AboutComponent } from './components/pages/about/about.component';
-import { ChampionsComponent } from './components/pages/champions/champions.component';
+import { ChampionComponent } from './components/pages/champions/champions.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutComponent},
-  {path: 'champions', component: ChampionsComponent}
+  {path: 'champions', component: ChampionComponent}
 ];
