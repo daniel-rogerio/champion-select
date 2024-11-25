7e26ae3c43fa4d1695a8d554199bce8d971da26e
daniel-rogerio <danielr.xp@gmail.com>
2024-11-24 21:47:28 -0300

    Add the animations provider.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,6 +10,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
+    provideAnimationsAsync()
   ]
 };
