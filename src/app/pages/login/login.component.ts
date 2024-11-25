438a967d6e247b3e64213360a6a506439a0b010c
daniel-rogerio <danielr.xp@gmail.com>
2024-11-24 21:46:53 -0300

    Create the login component.

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,17 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [MatCardModule, MatTabsModule, MatInputModule, MatButtonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LoginComponent {
+
+}
